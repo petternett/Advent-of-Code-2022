24ebcad37b999e0b43034daf986c7e9380d24437
Petter Barhaugen <petter@petternett.no>
2022-12-09 22:37:38 +0100

    finished 6b

--- 6/6b.py
+++ 6/6b.py
@@ -2,7 +2,7 @@ with open("input") as fp:
     data = fp.read()[:-1]
 
     last = []
-    check = 4
+    check = 14
     for i, char in enumerate(data):
         last = data[i:i+check]
         if len(set(last)) == check:
